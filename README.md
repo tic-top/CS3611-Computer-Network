203461ca57f69aaf76b81cadcf118388ef0f87b1
jiayilin <jiayilin_kirp@sjtu.edu.cn>
2022-04-02 16:48:14 +0800

    lab1

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # CS3611-Computer-Network
+
+time: 2022/4/2
+
+author: tic-top
--- lab2/hw3.sh
+++ lab2/hw3.sh
@@ -14,9 +14,3 @@ sh ovs-ofctl add-flow s3 "in_port=1 actions=output:2,4"
 sh ovs-ofctl add-flow s3 "in_port=2 actions=output:4,1"
 sh ovs-ofctl add-flow s3 "in_port=3 actions=drop"
 sh ovs-ofctl add-flow s3 "in_port=4 actions=output:1,2"
-
-
-
-
-
-
